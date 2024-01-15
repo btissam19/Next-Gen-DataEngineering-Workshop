bc80834196f8df643979f65b6b45c544650d0d94
Ibtissam <btissam1234btissam12@gmail.com>
2024-01-15 17:40:29 +0100

    the last version

--- index.js
+++ index.js
@@ -1,25 +1,38 @@
 function sendMail() {
+  var name = document.getElementById("name").value;
+  var email = document.getElementById("email").value;
+  var message = document.getElementById("message").value;
+  if (name === "" || email === "" || message === "") {
+    document.querySelector('.error-message').innerText = 'Please fill in all fields.';
+    document.querySelector('.error-message').style.display = 'block';
+    return;
+  }
+
   var params = {
-    name: document.getElementById("name").value,
-    email: document.getElementById("email").value,
-    message: document.getElementById("message").value,
+    name: name,
+    email: email,
+    message: message
   };
 
   const serviceID = "service_rx7xzdc";
   const templateID = "template_ax92u6f";
 
-    emailjs.send(serviceID, templateID, params)
-    .then(res=>{
-        document.getElementById("name").value = "";
-        document.getElementById("email").value = "";
-        document.getElementById("message").value = "";
-        console.log(res);
-        alert("Your message sent successfully!!")
-
+  emailjs.send(serviceID, templateID, params)
+    .then(res => {
+      document.getElementById("name").value = "";
+      document.getElementById("email").value = "";
+      document.getElementById("message").value = "";
+      document.querySelector('.sent-message').style.display = 'block';
+      document.querySelector('.error-message').style.display = 'none';
     })
-    .catch(err=>console.log(err));
-
+    .catch(err => {
+      console.log(err);
+      document.querySelector('.error-message').innerText = 'Failed to send message. Please try again later.';
+      document.querySelector('.error-message').style.display = 'block';
+      document.querySelector('.sent-message').style.display = 'none';
+    });
 }
+
 
 
 
@@ -39,7 +52,7 @@ function sendMail() {
    const seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
 
    // Afficher le compte à rebours dans l'élément avec l'ID "countdown"
-   document.getElementById("countdown").innerHTML =
+   document.getElementById("countdown").innerHTML = "Event start in       "+
      days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
 
    // Si le compte à rebours est terminé, afficher un message
